23f0a1b1b473fa53b716049d83daae185066fc32
jjoaom <joaomarcos.aquino@hotmail.com>
2026-05-21 14:48:06 -0300

    Add: Code Review Comment adicionar-tarefas.js

--- Cronoflex/public/projetos/assets/scripts/adicionar-tarefas.js
+++ Cronoflex/public/projetos/assets/scripts/adicionar-tarefas.js
@@ -47,6 +47,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function obterID() {
         /* Indicado utilizar Number.parseint por possuir mais benefícios de código. 
         ECMA Script 2015 introduziu métodos estaticos no construtor Number, tornando-o mais consistente, organizado e alinhado as praticas modernas de engenharia de software. Ao user o Number ao invés de chamadas globais, reduzimos o risco de de conflitos de nomeclatura.        */
+        /* A função obterID deveria estar no topo do arquivo, como o javascript lê seu código de cima para baixo, todo o conteúdo do arquivo é carregado(inclusive a chamada), para somente depois chegar a função para cumprir seu propósito. Isso deixa o código mal otimizado e piora sua manutenibilidade. */
         let id = parseInt(localStorage.getItem("id")) || 0;
         id += 1;
         localStorage.setItem("id", id);
