76b01055a5ef8f5458201527bcd41e63ae7d45ea
jjoaom <joaomarcos.aquino@hotmail.com>
2026-05-21 13:12:18 -0300

    Add: Code Review Comment server.js

---
+++ Cronoflex/functions/server.js
@@ -0,0 +1,4 @@
+/*
+O arquivo de server se encontra vazio. É incerto dizer como o deploy desta aplicação pode ser feita por falta de código e instruções de repositório. 
+Sugiro refatorar para práticas modernas como React, Vue, Angular ou outro framework moderno. Ou servir via NGINX ou Caddy diretamente a aplicação estática.
+*/
